8d457fe2d05e83001ebfc2783bec984629b14e37
Tesla <tesla.a.lyon@gmail.com>
2026-04-29 18:55:34 -0700

    Added some projects content (to be finished) and placeholder

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./Projects.css";
 import onc from "../../assets/ournationalconversation_screengrab.png"
 import habitTracker from "../../assets/habit_tracker_figmascreengrab.png"
-
+import portfolio from "../../assets/developer_portfolio.png"
 
 function Projects() {
     
@@ -17,13 +17,23 @@ function Projects() {
             title: "Habit Tracker",
             description: "An in-progress CRUD app",
             imgUrl: habitTracker
+        },
+        {
+            title: "Portfolio",
+            description: "My portfolio site (you're here!)",
+            imgUrl: portfolio
         }
     ]
 
     return (
-    <section className="banner" id="projects">
+    <section className="project" id="projects">
         <Container>
-            <h1>This is where my projects will go.</h1>
+            <Row>
+                <Col>
+                <h2>Projects</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </Col>
+            </Row>
         </Container>
     </section>
     );
